c8d6ff9bc4bfa9e6e1e65c522d62b3bc8ada2970
TheAlternateDoctor <thealternatedoc@gmail.com>
2023-10-16 12:03:22 +0200

    Started working on no lives version

--- compileFork.sh
+++ compileFork.sh
@@ -1,3 +1,4 @@
 java -jar ../tickompiler.jar c tickflow bin
 java -jar ../tickompiler.jar p bin ../code.bin
-mv -f C00.bin ~/.var/app/org.citra_emu.citra/data/citra-emu/sdmc/rhmm
+mv -f C00.bin ~/.local/share/citra-emu/sdmc/rhmm
+killall -9 citra-qt
