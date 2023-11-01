eb952033a4191b9f8e531ba9e78513450656002d
TheAlternateDoctor <thealternatedoc@gmail.com>
2023-11-02 00:53:07 +0100

    Replaced Prologue

--- compileFork.sh
+++ compileFork.sh
@@ -1,4 +1,4 @@
 java -jar ../tickompiler.jar c tickflow bin
-java -jar ../tickompiler.jar p bin ../code.bin
+java -jar ../tickompiler.jar p bin base.bin
 mv -f C00.bin ~/.local/share/citra-emu/sdmc/rhmm
 killall -9 citra-qt
