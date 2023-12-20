e833e38365349d6aabd78e1b26bc2a9181f954d3
tarp2020 <54559807+tarp2020@users.noreply.github.com>
2023-12-19 22:53:40 -0800

    Umm lets just see what happens

---
+++ compileFork.ps1
@@ -0,0 +1,3 @@
+﻿java -jar ..\tickompiler.jar c tickflow bin
+java -jar ..\tickompiler.jar p bin\ ..\base.bin
+mv -force C00.bin $Env:APPDATA\Citra\sdmc\rhmm
---
+++ tickompiler.bat
@@ -0,0 +1,4 @@
+java -jar tickompiler.jar c tickflow bin
+java -jar tickompiler-2.jar p bin base.bin
+mv -force C00.bin $Env:APPDATA\Citra\sdmc\rhmm
+pause
